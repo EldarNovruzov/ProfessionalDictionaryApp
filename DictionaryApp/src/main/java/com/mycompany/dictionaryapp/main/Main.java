61b791bb73a4d64b3aa4c403e5594fc735383724
Eldar <eldarnovruzov99@gmail.com>
2019-02-06 16:59:21 +0400

    Final Changed

--- DictionaryApp/src/main/java/com/mycompany/dictionaryapp/main/Main.java
+++ DictionaryApp/src/main/java/com/mycompany/dictionaryapp/main/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.dictionaryapp;
+package com.mycompany.dictionaryapp.main;
 
 import com.mycompany.dictionaryapp.utility.MenuHelper;
 
